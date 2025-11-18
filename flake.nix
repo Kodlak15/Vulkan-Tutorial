bd04e7ec0e7c0bc32c1bb7a9b05ea13803725bec
Kodlak15 <stanlcod15@protonmail.com>
2025-11-18 12:45:10 -0800

    Still dealing with some rough edges with vulkan profiles (try compiling chapter 35)

--- flake.nix
+++ flake.nix
@@ -18,46 +18,80 @@
         pkgs,
         system,
         ...
-      }: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            cmake
-            ninja
-            catch2
-            boost
-            vulkan-headers
-            vulkan-loader
-            vulkan-validation-layers
-            vulkan-tools
-            vulkan-tools-lunarg
-            glslang
-            glfw
-            glm
-            wayland
-            libxkbcommon
-            shaderc
-            tinyobjloader
-            stb
-            nlohmann_json
-            xorg.libXxf86vm
-            shader-slang
-            ktx-tools
-            tinygltf
+      }: let
+        vulkan-profiles = pkgs.stdenv.mkDerivation rec {
+          pname = "vulkan-profiles";
+          version = pkgs.vulkan-headers.version;
+
+          cmakeFlags = [
+            "-DVULKAN_HEADERS_INSTALL_DIR=${pkgs.vulkan-headers}"
           ];
 
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
-            vulkan-loader
-            vulkan-validation-layers
-            wayland
-            libxkbcommon
-            glfw
-            glm
-            tinyobjloader
-            stb
+          nativeBuildInputs = with pkgs; [cmake ninja python3];
+          buildInputs = with pkgs; [
+            vulkan-headers
+            vulkan-utility-libraries
+            valijson
+            (jsoncpp.override {enableStatic = true;})
+          ];
+
+          src = pkgs.fetchFromGitHub {
+            owner = "KhronosGroup";
+            repo = "Vulkan-Profiles";
+            rev = "vulkan-sdk-${version}";
+            hash = "sha256-u6Q6nugoppuiElDWtO9F4XJsgPFYpht0EhNrvOe/bhE=";
+          };
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs;
+            [
+              cmake
+              ninja
+              catch2
+              boost
+              vulkan-headers
+              vulkan-loader
+              vulkan-validation-layers
+              vulkan-tools
+              vulkan-tools-lunarg
+              vulkan-extension-layer
+              vulkan-utility-libraries
+              glslang
+              glfw
+              glm
+              wayland
+              libxkbcommon
+              shaderc
+              tinyobjloader
+              stb
+              nlohmann_json
+              xorg.libXxf86vm
+              shader-slang
+              ktx-tools
+              tinygltf
+            ]
+            ++ [vulkan-profiles];
+
+          buildInputs = with pkgs; [
             nlohmann_json
-            ktx-tools
-            tinygltf
-          ]);
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs;
+            [
+              vulkan-loader
+              vulkan-validation-layers
+              wayland
+              libxkbcommon
+              glfw
+              glm
+              tinyobjloader
+              stb
+              nlohmann_json
+              ktx-tools
+              tinygltf
+            ]
+            ++ [vulkan-profiles]);
 
           shellHook = ''
             exec zsh -c zellij
